2411dbfd18edd4ead42081a5d6f0a6771c1f385f
ujklm23 <ridwanrasyid345@gmail.com>
2023-10-02 07:31:34 +0700

    add: day 3 & 4

---
+++ js/contact.js
@@ -0,0 +1,24 @@
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const data = {
+    name: e.target.name.value,
+    email: e.target.email.value,
+    phoneNumber: e.target['phone-number'].value,
+    subject: e.target.subject.value,
+    message: e.target.message.value,
+  };
+
+  for (const property in data) {
+    if (data[property] == '') {
+      alert('Form must be filled');
+      return;
+    }
+  }
+
+  const a = document.createElement('a');
+  a.href = `https://mail.google.com/mail/?view=cm&fs=1&to=ridwanrasyid345@gmail.com&su=${data.subject}&body=Halo, nama saya ${data.name}.${data.message} .Tolong kontak saya pada email ${data.email} atau melalui telepon ${data.phoneNumber}`;
+  a.target = '_blank';
+
+  a.click();
+}
---
+++ js/project.js
@@ -0,0 +1,91 @@
+const projects = [];
+
+document.getElementById('upload-image').addEventListener('change', e => {
+  document.querySelector('.image__name').innerHTML = e.target.files[0].name;
+});
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const technologies = ['node-checkbox', 'react-checkbox', 'nextjs-checkbox', 'typescript-checkbox'];
+  const inputImage = e.target['upload-image'].files[0];
+
+  if (!inputImage) {
+    alert('Image is Null');
+    return;
+  }
+
+  const imageURL = URL.createObjectURL(inputImage);
+
+  const project = {
+    projectName: e.target['project-name'].value,
+    startDate: e.target['start-date'].value,
+    endDate: e.target['end-date'].value,
+    // time: new Date(new Date(e.target['end-date'].value) - new Date(e.target['start-date'].value)).getDate(),
+    description: e.target['description'].value,
+    technologies: technologies.map(tech => e.target[tech].checked && tech).filter(tech => tech),
+    image: imageURL,
+  };
+
+  if (new Date(project.startDate) > new Date(project.endDate)) {
+    alert('Duration cannot be minus');
+    return;
+  }
+
+  for (const property in project) {
+    if (project[property].length < 1) {
+      alert('Form must be filled');
+      return;
+    }
+  }
+
+  projects.push(project);
+
+  renderHTML();
+}
+
+function renderHTML() {
+  const html = projects.map(
+    (project, idx) => /*html*/ `
+    <div class="card">
+      <img src="${project.image}" alt="project" class="project__image" />
+      <div class="project__header">
+        <a href="detail-project.html" target="_blank">
+          <h3>${project.projectName}</h3>
+          <p>duration: ${countTime(project.startDate, project.endDate)}</p>
+        </a>
+      </div>
+
+      <div class="project__description">
+        ${project.description}
+      </div>
+
+      <div class="project__icons">
+        ${project.technologies
+          .map(tech => `<img src="images/${tech}.png" alt="${tech}" title="${tech.split('-')[0]}" />`)
+          .join('')}
+      </div>
+
+      <div class="project__action">
+        <button>Edit</button>
+        <button onClick="handleDelete(${idx})">Delete</button>
+      </div>
+    </div>`
+  );
+
+  const cardWrapper = document.querySelector('.card__wrapper');
+  cardWrapper.innerHTML = html.join('');
+}
+
+function countTime(startDate, endDate) {
+  const diff = new Date(endDate) - new Date(startDate);
+  let months = new Date(diff).getMonth();
+  let date = new Date(diff).getDate();
+
+  return `${months > 0 ? months + ' months' : ''} ${date} days`;
+}
+
+function handleDelete(idx) {
+  projects.splice(idx, 1);
+  renderHTML();
+}
