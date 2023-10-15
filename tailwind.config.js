3740537930b89f34dea0553e7d08b72eae261d98
ujklm23 <ridwanrasyid345@gmail.com>
2023-10-15 18:05:27 +0700

    change style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#111727',
-        secondary: '#e3e7eb',
+        // primary: '#111727',
+        // secondary: '#e3e7eb',
+        secondary: '#dddddd',
+        primary: '#141414',
+        tertiary: '#fe4b05',
+      },
+      screens: {
+        xs: '540px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
       },
     },
   },
